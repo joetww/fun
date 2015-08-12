f03eadd4013243773275a343e82f32964e7ad431
游小爸 <joetww@users.noreply.github.com>
2015-08-12 10:40:17 +0800

    說明一些東西

--- youtube_mpg123play.sh
+++ youtube_mpg123play.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#youtube-dl http://youtube-dl.org/
+#新版的youtube-dl 參數-b被拿掉囉，所以若是不能使用請自己修正
+
 url="https://youtu.be/uuwfgXD8qV8" && \
 wget -q -O - `youtube-dl -b -g ${url}`| \
 ffmpeg -i - -f mp3 -vn -acodec libmp3lame -| \
