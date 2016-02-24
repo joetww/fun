d5d15e00770c8ce0c0fab8583e98096040f8237c
joetww <joetww@users.noreply.github.com>
2016-02-24 16:07:08 +0800

    Update cUrlKobe.sh

--- cUrlKobe.sh
+++ cUrlKobe.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#靠北工程師
+# 靠北工程師
+# 簡單用lynx 
+#  lynx --dump -width=160 http://wttr.in/taipei 
 
 curl curl.kobe.ga/img -d "msg=$((lynx --dump -width=160 http://wttr.in/taipei | head -n 7  && echo "程序組合 By Joe Yue" ) | sed ':a;N;$!ba;s/\n/%0a/g')"
