11f7ed6eed0dab77f6039be2ef78033e85c04026
游小爸 <joetww@users.noreply.github.com>
2018-07-19 15:57:01 +0800

    Create changeDNSSetting.ps1

---
+++ changeDNSSetting.ps1
@@ -0,0 +1,6 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Get-NetAdapter | SELECT Name, ifIndex, Status | where status -eq 'up' | ForEach-Object {
+	echo $_.Name
+	Set-DnsClientServerAddress -InterfaceIndex $_.ifIndex -ServerAddresses("192.168.99.6", "8.8.8.8", "101.101.101.101")
+}
