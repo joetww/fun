78e072fc7cbb4c27a01e8dd1fc40fda3e4ceab40
游小爸 <joetww@users.noreply.github.com>
2020-12-07 12:42:10 +0800

    Update ProxyAutoConfiguration.js

--- ProxyAutoConfiguration.js
+++ ProxyAutoConfiguration.js
@@ -3,7 +3,17 @@ function FindProxyForURL(url, host) {
     // use proxy for specific domains
     if (shExpMatch(host, "ifconfig.co|vcsa.phtmachine.tk|*.google.com|*.facebook.com"))
         return "PROXY 192.168.99.166:8888";
-
+    // If the requested website is hosted within the internal network, send direct.
+    if (isPlainHostName(host) ||
+        shExpMatch(host, "*.local") ||
+        isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+        isInNet(dnsResolve(host), "172.16.0.0",  "255.240.0.0") ||
+        isInNet(dnsResolve(host), "192.168.0.0",  "255.255.0.0") ||
+        isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0"))
+    return "DIRECT";
+    
     // by default use no proxy
     return "DIRECT";
 }
+
+// https://findproxyforurl.com/example-pac-file/
