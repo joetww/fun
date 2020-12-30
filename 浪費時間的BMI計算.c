3c04276d277449e46d0a4b81bbb5d5a8e627b71c
游小爸 <joetww@users.noreply.github.com>
2020-12-30 15:44:36 +0800

    Create 浪費時間的BMI計算.c
    joeyue@ms-7a74:~/bmi$ ./main | bash
    Input Your Height: 182
    Input Your Weight: 110
    33.23

---
+++ 浪費時間的BMI計算.c
@@ -0,0 +1,29 @@
+/*
+  我不想直接算出BMI
+  我只想列出計算式，再送給bash去找bc來算
+
+$ ./main | bash
+Input Your Height: 182
+Input Your Weight: 110
+33.23
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    float Hight;
+    float Weight;
+    float HightKG;
+    float BMI;
+    fprintf(stderr, "Input Your Height: ");
+    scanf("%f",&Hight);
+    fprintf(stderr, "Input Your Weight: ");
+    scanf("%f",&Weight);
+    HightKG=(Hight * 0.01);
+    BMI=(Weight/(HightKG*HightKG));
+    printf("#Your BMI Size is %f\n",BMI);
+    printf("echo \"scale = 2; %f / ( %f / 100 )^2\" | $(which bc)\n", Weight, Hight);
+    return 0;
+}
